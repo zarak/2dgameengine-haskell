ebe964f0078e36a6440d1fc63daea35de94f2a09
Zarak <7254237+zarak@users.noreply.github.com>
2021-02-07 20:52:06 +0500

    WIP: consistent framerate

--- app/Main.hs
+++ app/Main.hs
@@ -3,26 +3,40 @@ module Main where
 
 import qualified Common                 as C
 import qualified SDL
+import qualified SDL.Raw.Timer          as SDLTimer
 
 import           Control.Monad          (unless)
 import           Control.Monad.IO.Class (MonadIO)
 import           Data.IORef
 import           Foreign.C.Types        (CInt)
 import           Linear
+import Data.Word (Word32)
 
-newtype World = World
-  { object :: V2 CInt
+data World = World
+  { player :: V2 CInt
+  , ticksLastFrame :: Word32
   }
 
+rectangleSize :: V2 CInt
+rectangleSize = V2 20 20
+
+framesPerSecond :: Float
+framesPerSecond = 60
+
+frameTargetTime :: Float
+frameTargetTime = 1000 / framesPerSecond;
+
 initialWorld :: World
-initialWorld = World { object = V2 10 10 }
+initialWorld = World { player = V2 10 10
+                     , ticksLastFrame = 0
+                     }
 
 main :: IO ()
 main = C.withSDL $ C.withSDLImage $ do
   C.setHintQuality
   C.withWindow "Test SDL" (640, 480) $ \w ->
     C.withRenderer w $ \r -> do
-      worldRef <- newIORef (World (V2 10 10))
+      worldRef <- newIORef initialWorld
       appLoop r worldRef
 
 appLoop :: SDL.Renderer -> IORef World -> IO ()
@@ -31,12 +45,25 @@ appLoop renderer worldRef = do
 
   world <- readIORef worldRef
 
-  let qPressed = any eventIsQPress events
+  t <- SDLTimer.getTicks
+
+  let timeToWait = floor frameTargetTime - (t - ticksLastFrame world)
+
+      qPressed = any eventIsQPress events
 
       doRender :: World -> IO ()
       doRender = render renderer
 
-      world' = updateWorld events world
+
+      timeToWait' = if timeToWait > 0 && timeToWait < floor frameTargetTime
+         then timeToWait
+         else 0
+
+  SDL.delay timeToWait'
+
+  t' <- SDLTimer.getTicks
+  let deltaTime = (t' - ticksLastFrame world) `div` 1000
+      world' = updateWorld events world deltaTime
 
   doRender world'
 
@@ -44,12 +71,14 @@ appLoop renderer worldRef = do
 
   unless qPressed (appLoop renderer worldRef)
 
-updateWorld :: [SDL.Event] -> World -> World
-updateWorld _ world = World { object = object world + 1 }
+updateWorld :: [SDL.Event] -> World -> Word32 -> World
+updateWorld _ world dT = World { player = player world + 1
+                            , ticksLastFrame = ticksLastFrame world 
+                            }
 
 drawWorld :: MonadIO m => SDL.Renderer -> World -> m ()
 drawWorld renderer world = do
-  let position = object world
+  let position = player world
   SDL.rendererDrawColor renderer SDL.$= V4 255 255 255 255
   let rect = SDL.Rectangle (SDL.P position) rectangleSize
   SDL.drawRect renderer $ Just rect
@@ -71,6 +100,3 @@ render renderer world = do
   drawWorld renderer world
   
   SDL.present renderer
-
-rectangleSize :: V2 CInt
-rectangleSize = V2 20 20
