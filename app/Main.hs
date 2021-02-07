545dec0ca57cc7702d7fe33e2e1ce961e8c59083
Zarak <7254237+zarak@users.noreply.github.com>
2021-02-07 14:02:53 +0500

    Setup render and window plus cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import qualified MyLib (someFunc)
+import qualified SDL
+import qualified Common as C
+
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Loops    (iterateUntilM)
+import Data.Foldable (foldl')
 
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+main = C.withSDL $ C.withSDLImage $ do
+  C.setHintQuality
+  C.withWindow "Test SDL" (640, 480) $ \w ->
+    C.withRenderer w $ \r -> do
+
+      putStrLn "Hello"
