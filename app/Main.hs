61b1772fe2a5e533b5c72c3a2c32ca7ec65972a1
Zarak <7254237+zarak@users.noreply.github.com>
2021-02-07 16:33:45 +0500

    Unable to draw to screen

--- app/Main.hs
+++ app/Main.hs
@@ -2,20 +2,19 @@
 module Main where
 
 import qualified Common                 as C
-import qualified MyLib                  (someFunc)
 import qualified SDL
 
 import           Control.Monad.IO.Class (MonadIO)
-import           Control.Monad.Loops    (iterateUntilM)
-import           Data.Foldable          (foldl')
 import           Linear
 import Control.Monad (unless)
 import Foreign.C.Types (CInt)
-import GHC.IO (liftIO)
 
 newtype World = World
   { object :: V2 CInt
   }
+
+initialWorld :: World
+initialWorld = World { object = V2 10 10 }
 
 main :: IO ()
 main = C.withSDL $ C.withSDLImage $ do
@@ -33,10 +32,13 @@ appLoop renderer = do
       doRender :: World -> IO ()
       doRender = render renderer
 
+  doRender $ foldr updateWorld initialWorld events
+
   unless qPressed (appLoop renderer)
 
-updateWorld :: SDL.Event -> [SDL.Event] -> World
-updateWorld _ = error "not implemented"
+updateWorld :: SDL.Event -> World -> World
+-- updateWorld _ world = World { object = object world + 1 }
+updateWorld _ = id
 
 drawWorld :: MonadIO m => SDL.Renderer -> World -> m ()
 drawWorld renderer world = do
@@ -66,4 +68,3 @@ rectangleSize = V2 10 10
 
 rectanglePosition :: V2 CInt
 rectanglePosition = V2 0 0
-
